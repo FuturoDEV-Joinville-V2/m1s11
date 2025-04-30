c4c188f74bc5720b8618c0d6f57264068bfb0365
Davi Saldanha <davisaldanha.ti@gmail.com>
2025-04-30 20:32:58 -0300

    Implement methods validatePassword, findByUsername, loadUserByUsername

--- src/main/java/com/example/exampleSecurity/service/UserService.java
+++ src/main/java/com/example/exampleSecurity/service/UserService.java
@@ -3,6 +3,9 @@ package com.example.exampleSecurity.service;
 import com.example.exampleSecurity.entity.User;
 import com.example.exampleSecurity.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,6 +13,23 @@ public class UserService {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+
+    public User findByUsername(String username){
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public boolean validatePassword(String rawPassword, String encodedPassword){
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        return passwordEncoder.matches(rawPassword, encodedPassword);
+    }
+
+    public UserDetails loadUserByusername(String username){
+        return userDetailsService.loadUserByUsername(username);
+    }
 
     public void registerUser(User user){
         if (!userRepository.existsByUsername(user.getUsername())) {
